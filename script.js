8e4155591ab0db7c585089fd958b88d4dbf43699
Hatice GAZEL <98157466+gazellhatice@users.noreply.github.com>
2022-04-07 20:07:03 +0300

    text

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        if(this.scroll > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    })
+});
+
+// slide-up script
+$('.scroll-up-btn').click(function(){
+    $('html').animate({scrollTop: 0});
+});
+
+// typing animation script
+var typed = new Typed(".typing", {
+    strings: ["Software Engineer","Developer","Designer"],
+    typeSpeed: 100,
+    backSpeed:60,
+    loop: true
+});
