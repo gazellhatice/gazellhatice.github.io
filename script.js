1d3ec07e601c03904d8828248817ea0d2d804797
Hatice GAZEL <98157466+gazellhatice@users.noreply.github.com>
2022-06-12 03:53:41 +0300

    text

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+data = [{
+    "projectName":"Kuaförüm Elimde",
+    "date": "(06/2022) <br>",
+    "details": "   Kuaförleri arayıp randevu saati beklemek yerine anında telefondan nerede, hangi kuaförün boş olduğunu öğrenebileceğiz. <br>" +
+                "-Bu sayede hem zamandan kazanıp hem de en iyi ve en yakın kuaförü kolaylıkla öğrenebileceğiz."
+    
+},
+
+
+{
+    "projectName":"     ",
+    "date": "     ",
+    "details": "     ",
+
+},
+{
+    "projectName":"KİDSİ",
+    "date": "(04/2022) <br>",
+    "details": "   Çocuklar telefona girdiği anda onun ebeveyn olmadığını anlayıp gezintisini yönetebilecek yapay zeka tabanlı, akıllı kişisel asistan <br> " +
+                "-KİDSİ Dijital Ebeveyn"
+    
+}]
+
+
 $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20){
@@ -17,6 +41,15 @@ $(document).ready(function(){
         $('.menu-btn i').toggleClass("active");
 
     });
+
+    //projects script
+    for (let i = 0; i < data.length; i++) {
+        const f = data[i];
+        var tblRow = "<tr>" + "<td>" + f.projectName + "</td>" + 
+                "<td>" + f.date + "</td>" + "<td>" + f.details + "</td></tr>" 
+                $(tblRow).appendTo("#userdata tbody");
+    }
+
 });
 
 // slide-up script
@@ -31,5 +64,4 @@ var typed = new Typed(".typing", {
     backSpeed:60,
     loop: true
 });
-
 
